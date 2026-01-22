a3eee068f6424c56f14e6e0b960704e25f80018d
melinpspt <melinpspt009@gmail.com>
2026-01-22 14:21:57 +0700

    a

--- admin_melinda/batal_transaksi_melinda.php
+++ admin_melinda/batal_transaksi_melinda.php
@@ -1,25 +1,17 @@
 <?php
-session_start();
 include "../config_melinda/koneksi_melinda.php";
 
-if ($_SESSION['role_melinda'] != 'admin') {
-    header("location:../auth_melinda/login_melinda.php");
-    exit;
-}
-
 $id = $_GET['id'];
-$tanggal_hari_ini = date('Y-m-d');
 
+// update status jadi DIKEMBALIKAN + isi tanggal kembali
 mysqli_query($koneksi_melinda, "
-    UPDATE peminjaman_melinda
-    SET 
-        status_melinda='dibatalkan',
-        tanggal_kembali_melinda='$tanggal_hari_ini'
-    WHERE id_peminjaman_melinda='$id'
+    UPDATE peminjaman_melinda SET
+        status_melinda = 'dikembalikan',
+        tanggal_kembali_melinda = CURDATE()
+    WHERE id_peminjaman_melinda = '$id'
 ");
 
 echo "<script>
-    alert('Transaksi berhasil dibatalkan');
+    alert('Buku berhasil dikembalikan');
     window.location='data_peminjaman_melinda.php';
 </script>";
-?>
--- admin_melinda/data_peminjaman_melinda.php
+++ admin_melinda/data_peminjaman_melinda.php
@@ -7,7 +7,7 @@ include "../config_melinda/koneksi_melinda.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Peminjaman</title>
 </head>
 
 <body>
@@ -23,15 +23,19 @@ include "../config_melinda/koneksi_melinda.php";
             <th>Tanggal Pinjam</th>
             <th>Tanggal Kembali</th>
             <th>Status</th>
-            <th>aksi</th>
+            <th>Aksi</th>
         </tr>
+
         <?php
         $no_melinda = 1;
         $query_melinda = mysqli_query($koneksi_melinda, "
-    SELECT * FROM peminjaman_melinda
-    JOIN anggota_melinda ON peminjaman_melinda.id_anggota_melinda = anggota_melinda.id_anggota_melinda
-    JOIN buku_melinda ON peminjaman_melinda.id_buku_melinda = buku_melinda.id_buku_melinda
-");
+            SELECT * FROM peminjaman_melinda
+            JOIN anggota_melinda 
+                ON peminjaman_melinda.id_anggota_melinda = anggota_melinda.id_anggota_melinda
+            JOIN buku_melinda 
+                ON peminjaman_melinda.id_buku_melinda = buku_melinda.id_buku_melinda
+        ");
+
         while ($data_melinda = mysqli_fetch_assoc($query_melinda)) {
             ?>
             <tr>
@@ -39,31 +43,28 @@ include "../config_melinda/koneksi_melinda.php";
                 <td><?= $data_melinda['nama_anggota_melinda']; ?></td>
                 <td><?= $data_melinda['judul_buku_melinda']; ?></td>
                 <td><?= $data_melinda['tanggal_pinjam_melinda']; ?></td>
-                <td><?= $data_melinda['tanggal_kembali_melinda']; ?></td>
+                <td><?= $data_melinda['tanggal_kembali_melinda'] ?: '-'; ?></td>
                 <td>
                     <?php
                     if ($data_melinda['status_melinda'] == 'dipinjam') {
                         echo "<span style='color:orange;'>Dipinjam</span>";
                     } elseif ($data_melinda['status_melinda'] == 'dikembalikan') {
                         echo "<span style='color:green;'>Dikembalikan</span>";
-                    } elseif ($data_melinda['status_melinda'] == 'dibatalkan') {
-                        echo "<span style='color:red;'>Dibatalkan</span>";
                     }
                     ?>
                 </td>
-
                 <td>
-                    <?php if ($data_melinda['status_melinda'] != 'dibatalkan') { ?>
+                    <?php if ($data_melinda['status_melinda'] == 'dipinjam') { ?>
                         <a href="edit_transaksi_melinda.php?id=<?= $data_melinda['id_peminjaman_melinda']; ?>">
                             Edit
                         </a>
                         |
                         <a href="batal_transaksi_melinda.php?id=<?= $data_melinda['id_peminjaman_melinda']; ?>"
                             onclick="return confirm('Batalkan transaksi ini?')">
-                            Batal
+                            Kembalikan
                         </a>
                     <?php } else { ?>
-                        <span style="color:red;">Dibatalkan</span>
+                        -
                     <?php } ?>
                 </td>
             </tr>
